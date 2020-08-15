dd4c7c12ebb2c6bf6c3a95eca95ce7efd2ceeaf9
mushfiqurmahi <mushfiqurmahi2003@gmail.com>
2020-08-15 14:59:06 +0600

    app working good when input[0] is 1

--- prime.js
+++ prime.js
@@ -15,7 +15,7 @@ function inputVal(){
     let nums = input_str.split('-');
     console.log("[INPUT]", nums);
     if (nums.length === 1){
-        calculate(2, nums[0]) // 1 is not prime
+        calculate(1, nums[0]) // 1 is not prime (starting from 1)
     }
     else if (nums.length === 2){
         // start-end
@@ -27,13 +27,15 @@ function inputVal(){
     }
     else {
         // wrong input
+        console.error("[WRONG INPUT]");
         return;
     }
 }
 
 function calculate(start, end, step=1){
     console.log("[CACULATING]")
-    let all_primes = [];
+
+    let all_primes = []; //all prime number's array
     
     start = parseInt(start);
     end = parseInt(end);
@@ -41,32 +43,24 @@ function calculate(start, end, step=1){
     
     console.log(start, end, step);
     if (!(start && end && step && start>0 && end>0 && step>0 && start!=end)){
-        console.error("[WRONG INPUT]")
         //wrong input
+        console.error("[WRONG INPUT]");
         return;
     }
 
     // check if isPrime
     for (let i=start; i<=end; i+=step){
         if(isPrime(i, all_primes)){ // returns bool
-            console.info('[Prime]', i)
-            all_primes.push(i)
+            console.info('[Prime]', i);
+            all_primes.push(i);
         }
     }
 
-    // all primes found
-    // update dom;
-    let new_array = all_primes.slice(); //creating nwe array
+    console.log("[All Primes] ", all_primes);
+    // all primes are found --> all_primes array
 
-    for (let i=start; i<=end; i+=step){
-        if (i < new_array[0])
-            document.querySelector('.result-visual').innerHTML+=`<div><span>${i}</span></div>`;
-        else{ //if i==all_primes[0]
-            document.querySelector('.result-visual').innerHTML+=`<div data-prime><span>${i}</span></div>`;
-            new_array.shift();
-        }
-
-    }
+    // update dom
+    updateDom(start, end, step, all_primes)
 }
 
 function isPrime(num, all_primes){
@@ -84,5 +78,34 @@ function isPrime(num, all_primes){
             if (!(num%i)) return false; //if any i can divide num return false
         }
         return true; // if none returns false than its a prime number
+    }
+}
+
+function updateDom(start, end, step, all_primes){
+    
+    document.querySelector('.result-visual').innerHTML = '';
+    // clearing dom af there was any
+    
+    let new_array = all_primes.slice(); 
+    //copying array for efficiency (there are several method for copying an array)
+
+    for (let i=start; i<=end; i+=step){
+        if (!new_array.length || i < new_array[0]){
+            /*Checking if ...
+            1. new_array is empty (means no prime left)
+            2. or if not empty check current num is less than 1st element of primes 
+            */
+
+            // insert them as normal* numbers
+            document.querySelector('.result-visual').innerHTML+=`<div><span>${i}</span></div>`;
+        }
+        else{ 
+            // if i==all_primes[0] --> means its a prime number
+
+            // insert it as prime*  number
+            // and delete the 1st item(prime) of the array
+            document.querySelector('.result-visual').innerHTML+=`<div data-prime><span>${i}</span></div>`;
+            new_array.shift();
+        }
     }
 }
